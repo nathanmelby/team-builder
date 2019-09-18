b414b8c0e14a6801a761fd65f768101ffcf6ce00
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-18 16:15:52 -0500

    met mvp. Its ugly thoughj

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+
+const Form = props => {
+    const [teamMember, setTeamMember] = useState ({
+        name: "", 
+        email: "", 
+        role: ""
+    });
+
+    const handleChanges = event => {
+        console.log(teamMember);
+        setTeamMember({...teamMember, [event.target.name]: event.target.value});
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+        const newTeamMember = {
+            ...teamMember,
+            id:Date.now()
+        }
+        props.addNewTeamMember(newTeamMember);
+        setTeamMember({name: "", email: "", role: ""});
+    };
+
+    return (
+        <form onSubmit={submitForm}>
+            <label htmlFor="name">Name</label>
+            <input
+            name="name"
+            type="text"
+            value={teamMember.name}
+            onChange={handleChanges}
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+            name="email"
+            type="text"
+            value={teamMember.email}
+            onChange={handleChanges}
+            />
+
+            <label htmlFor="role">Role</label>
+            <input
+            name="role"
+            type="text"
+            value={teamMember.role}
+            onChange={handleChanges}
+            />
+
+            <button type="submit">Add Team Member</button>
+        </form>
+    )
+}
+
+export default Form;
---
+++ team-builder/src/components/TeamMembers.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const TeamMembers = props => {
+
+    return (
+    <div>
+        {props.teamMembers.map(teamMember => (
+            <div key={teamMember.id}>
+                <h2>{teamMember.name}</h2>
+                <p>{teamMember.role}</p>
+                <p>{teamMember.email}</p>
+            </div>
+        ))}
+    </div>
+    );
+};
+
+export default TeamMembers;
+  
